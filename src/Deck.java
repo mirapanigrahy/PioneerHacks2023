db639dc874507e690625f3a1ccb7449d9a4c8e65
Riya Panche <riya.m.panche@gmail.com>
2023-03-18 12:34:35 -0700

    create deck class

---
+++ src/Deck.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Deck {
+    ArrayList<Card> cards = new ArrayList<Card>();
+    private int numTerms = 0;
+
+    public Deck(int numTerms, ArrayList<Card> cards) {
+        this.numTerms = numTerms;
+        this.cards = cards;
+    }
+
+    public void addCard(Card card) {
+
+    }
+}
